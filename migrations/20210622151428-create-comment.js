1f119514d274adcff977a3d599fbef56edf1bfe7
beyzaerkan <beyzaerkan@icloud.com>
2021-06-29 12:31:05 +0300

    fixed migration conflict

--- migrations/20210622151428-create-comment.js
+++ migrations/20210622151428-create-comment.js
@@ -1,5 +1,4 @@
 module.exports = {
-<<<<<<< HEAD
   up: (queryInterface, Sequelize) => queryInterface.createTable('comments', {
     id: {
       type: Sequelize.INTEGER,
@@ -50,60 +49,4 @@ module.exports = {
     },
   }),
   down: (queryInterface, Sequelize) => queryInterface.dropTable('comments'),
-=======
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('comments', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      user_id: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        references: {
-          model: 'users',
-          key: 'id',
-        },
-        onUpdate: 'cascade',
-        onDelete: 'cascade',
-      },
-      content_review_id: {
-        type: Sequelize.INTEGER,
-        defaultValue: null
-      },
-      discussion_id: {
-        type: Sequelize.INTEGER,
-        defaultValue: null
-      },
-      parent_comment_id: {
-        type: Sequelize.INTEGER,
-        defaultValue: null,
-      },
-      text: {
-        type: Sequelize.STRING,
-        allowNull: false
-      },
-      is_spoiler: {
-        type: Sequelize.BOOLEAN,
-        defaultValue: false
-      },
-      created_at: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updated_at: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      deleted_at: {
-        type: Sequelize.DATE,
-      },
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('comments');
-  }
->>>>>>> 4f6f29afa8089e0df360bdfbde743d5d66ebcd2b
 };
