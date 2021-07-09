120f315997b2b08d46248b3223e7bb33df35fbd7
burakack <burak_ack@hotmail.com>
2021-07-09 06:23:23 +0300

    seeder added

---
+++ seeders/02210709052955-demo-wanted-content.js
@@ -0,0 +1,25 @@
+module.exports = {
+
+  up: async (queryInterface, Sequelize) => {
+    const wantedContents = [];
+    const status = ['Wants to read', 'Read', 'Didn\'t read'];
+    const myScores = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    for (let statu, myScore, index = 0; index < 6; index++) {
+      statu = status[Math.floor(Math.random() * 3)];
+      myScore = myScores[Math.floor(Math.random() * 10)];
+
+      wantedContents.push({
+        user_id: index + 1,
+        status: `${statu}`,
+        my_score: `${myScore}`,
+        created_at: new Date(),
+        updated_at: new Date(),
+
+      });
+    }
+
+    await queryInterface.bulkInsert('wanted_contents', wantedContents);
+  },
+
+};
