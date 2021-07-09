151fe1f1804f7da60bd9a144381052331956c3e5
burakack <burak_ack@hotmail.com>
2021-07-09 13:51:04 +0300

    bug fix and down add

--- seeders/02210709052955-demo-wanted-content.js
+++ seeders/02210709052955-demo-wanted-content.js
@@ -3,21 +3,22 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     const wantedContents = [];
     const status = ['Wants to read', 'Read', 'Didn\'t read'];
-    for (let statu, myScore, index = 0; index < 6; index++) {
-      statu = status[Math.floor(Math.random() * 3)];
-      myScore = Math.floor(Math.random() * 11);
-
+    for (let index = 0; index < 12; index++) {
       wantedContents.push({
         user_id: index + 1,
-        status: `${statu}`,
-        my_score: myScore,
-        created_at: new Date(),
+        content_id:index+1,
+        status: status[Math.floor(Math.random() * 3)],
+        my_score: Math.floor(Math.random() * 11),
+        created_at: new Date().setyear(2001),
         updated_at: new Date(),
-
       });
     }
 
     await queryInterface.bulkInsert('wanted_contents', wantedContents);
   },
-
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('wanted_contents',{
+      created_at: new Date().setyear(2001),
+    });
+  },
 };
