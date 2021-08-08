cc2bc0ae8fdabf3c1c3ce057832c38c11b9f9b3c
bssenoz <bsenoz@hotmail.com>
2021-08-08 03:59:01 +0300

    comment line deleted

--- src/routes/communities/index.js
+++ src/routes/communities/index.js
@@ -153,7 +153,6 @@ const update = async (req, res) => {
       });
       image_id = image.id;
     }
-    console.log(image_id);
     await models.communities.update({
       organizers,
       members,
