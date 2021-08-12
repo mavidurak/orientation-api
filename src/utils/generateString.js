377d2b719c0a69b87298e6735c3078b8cfbb2bac
saracalihan <saracalihan@gmail.com>
2021-08-12 16:23:49 +0300

    generateSlug function added

---
+++ src/utils/generateString.js
@@ -0,0 +1,22 @@
+import { randomBytes } from 'crypto';
+
+const generateSlug = (string) => {
+  // create slug from string
+  string = string.replace(/^\s+|\s+$/g, ''); // trim
+  string = string.toLowerCase();
+
+  // remove accents, swap ñ for n, etc
+  const from = 'àáäâèéëêìíïîòóöôùúüûñçğş·/_,:;&#';
+  const to =   'aaaaeeeeiiiioooouuuuncgs--------';
+  for (let i = 0, l = from.length; i < l; i++) {
+    string = string.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+  }
+
+  string = string.replace(/[^a-z0-9 -]/g, '') // remove invalid chars
+    .replace(/\s+/g, '-') // collapse whitespace and replace by -
+    .replace(/-+/g, '-'); // collapse dashes
+
+  return `${string}-${randomBytes(4).toString('hex')}`;
+};
+
+export default generateSlug;
