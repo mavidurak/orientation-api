55c4043b3666af9d20a5eb6a0b3aad824f4d8eef
Arifkarakilic <sarifkarakilic@gmail.com>
2021-09-10 17:32:43 +0300

    file upload and file, image services added

---
+++ src/routes/files/index.js
@@ -0,0 +1,44 @@
+import generateSlug from '../../utils/generateSlug'
+import models from '../../models'
+import HTTPError from '../../exceptions/HTTPError';
+import ImageService from '../../services/image';
+
+const upload = async (req, res, next) => {
+  try {
+    if (!req.files || Object.keys(req.files).length === 0) {
+      throw new HTTPError('No files were uploaded.', 400)
+    }
+    let file = req.files.file;
+    let path = await ImageService.uploadFile(file);
+    const image = await ImageService.createImage({
+      user_id: req.user.id,
+      name: file.name,
+      path,
+    });
+    return res.status(201).send({
+      image,
+    });
+  } catch (error) {
+    next(error);
+  }
+
+}
+const getFile = async (req, res, next) => {
+  const { path } = req.params;
+
+  try {
+    const files = await ImageService.getImageByPath(path);
+    return res.sendFile(__dirname.replace('src/routes/files', 'uploads/') + path);
+  } catch (err) {
+    next(err);
+  }
+
+}
+
+export default {
+  prefix: '/files',
+  inject: (router) => {
+    router.post('/upload', upload);
+    router.get('/:path', getFile);
+  },
+};
